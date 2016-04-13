e7acd5f59f0b9665716f2cc6dd0c54f59bffce26
xiao yue <xiao.yue13@student.xjtlu.edu.cn>
2016-04-14 02:35:36 +0800

    myRecipe1.1

--- app/src/main/java/com/example/yuexiao/myrecipe10/MainActivity.java
+++ app/src/main/java/com/example/yuexiao/myrecipe10/MainActivity.java
@@ -1,34 +1,38 @@
 package com.example.yuexiao.myrecipe10;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+import java.util.List;
 
-class TakePhotoPopWin extends PopupWindow {
+
+class PopWin extends PopupWindow {
 
     private Context mContext;
 
     private View view;
 
-    private Button btn_take_photo, btn_pick_photo, btn_cancel;
 
-
-
-    public TakePhotoPopWin(Context mContext, View.OnClickListener itemsOnClick) {
+    public PopWin(Context mContext) {
 
         this.view = LayoutInflater.from(mContext).inflate(R.layout.filter_pop, null);
         // TODO: 4/12/16 filter xml 界面设计与交互
@@ -59,7 +63,7 @@ class TakePhotoPopWin extends PopupWindow {
         this.setWidth(600);
         this.setFocusable(true);
 
-         //实例化一个ColorDrawable颜色为半透明
+        //实例化一个ColorDrawable颜色为半透明
         ColorDrawable dw = new ColorDrawable(0xb0000000);
         // 设置弹出窗体的背景
         this.setBackgroundDrawable(dw);
@@ -70,14 +74,32 @@ class TakePhotoPopWin extends PopupWindow {
     }
 }
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements android.view.View.OnClickListener {
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+    private List<View> views = new ArrayList<View>();
+
+    private LinearLayout iMenu;
+    private LinearLayout iPlan;
+    private LinearLayout iAlarm;
+    private LinearLayout iMe;
+
+    private ImageButton btnMenu;
+    private ImageButton btnPlan;
+    private ImageButton btnAlarm;
+    private ImageButton btnMe;
+
+    static private int currentItem;
+
+
     private AutoCompleteTextView autotext;
     private ArrayAdapter<String> arrayAdapter;
     final int CODE = 1;
     static private Boolean bool = true;
-    public static TakePhotoPopWin takePhotoPopWin;
+    public static PopWin popWin;
 
     private Context mContext;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,33 +117,176 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.iv_main_filter).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showPopFormBottom(v);
-            }
-        });
+
+        initView();
+        initViewPage();
+
+        initEvent();
+
+
 
     }
+
+    private void initView() {
+        viewPager = (ViewPager) findViewById(R.id.id_viewpage);
+
+        iPlan = (LinearLayout) findViewById(R.id.ll_tab_plan);
+        iAlarm = (LinearLayout) findViewById(R.id.ll_tab_alarm);
+        iMe = (LinearLayout) findViewById(R.id.ll_tab_me);
+        iMenu = (LinearLayout) findViewById(R.id.ll_tab_menu);
+
+        btnAlarm = (ImageButton) findViewById(R.id.ib_tab_alarm);
+        btnMe = (ImageButton) findViewById(R.id.ib_tab_me);
+        btnMenu = (ImageButton) findViewById(R.id.ib_tab_menu);
+        btnPlan = (ImageButton) findViewById(R.id.ib_tab_plan);
+
+        iMenu.setOnClickListener(this);
+        iMe.setOnClickListener(this);
+        iAlarm.setOnClickListener(this);
+        iPlan.setOnClickListener(this);
+
+    }
+
+    private void initViewPage() {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        views.add(layoutInflater.inflate(R.layout.menu, null));
+        views.add(layoutInflater.inflate(R.layout.plan, null));
+        views.add(layoutInflater.inflate(R.layout.alarm, null));
+        views.add(layoutInflater.inflate(R.layout.me, null));
+
+        pagerAdapter = new PagerAdapter() {
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(views.get(position));
+
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                View view = views.get(position);
+                container.addView(view);
+                return view;
+            }
+
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+
+                return arg0 == arg1;
+            }
+
+            @Override
+            public int getCount() {
+
+                return views.size();
+            }
+        };
+        viewPager.setAdapter(pagerAdapter);
+
+
+    }
+
+    private void initEvent() {
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+
+            public void onPageSelected(int arg0) {
+                currentItem=viewPager.getCurrentItem();
+                switch (currentItem) {
+                    case 0:
+                        resetImg();
+                        btnMenu.setImageResource(R.drawable.back4);
+                        findViewById(R.id.iv_main_filter).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                showPopFormBottom(v);
+                            }
+                        });
+                        break;
+                    case 1:
+                        resetImg();
+                        btnPlan.setImageResource(R.drawable.back4);
+                        break;
+                    case 2:
+                        resetImg();
+                        btnAlarm.setImageResource(R.drawable.back4);
+                        break;
+                    case 3:
+                        resetImg();
+                        btnMe.setImageResource(R.drawable.back4);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+
+            }
+
+
+        });
+
+
+    }
+
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
     public void showPopFormBottom(View view) {
-        takePhotoPopWin = new TakePhotoPopWin(this, onClickListener);
-        takePhotoPopWin.showAsDropDown(findViewById(R.id.ll_activitymain_1), 0, 0, Gravity.RIGHT);
-//        WindowManager.LayoutParams lp = getWindow().getAttributes();
-//        lp.alpha = 0.7F; //0.0-1.0
-//        getWindow().setAttributes(lp);
-        
+        popWin = new PopWin(this);
+        popWin.showAsDropDown(findViewById(R.id.tv_main_point), 0, 0, Gravity.RIGHT);
+
 
     }
 
 
-    private View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-        }
-    };
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_tab_menu:
+                viewPager.setCurrentItem(0);
+                resetImg();
+                btnMenu.setImageResource(R.drawable.back4);
+                break;
+            case R.id.ll_tab_me:
+                viewPager.setCurrentItem(1);
+                resetImg();
+                btnMe.setImageResource(R.drawable.back4);
+                break;
+            case R.id.ll_tab_alarm:
+                viewPager.setCurrentItem(2);
+                resetImg();
+                btnAlarm.setImageResource(R.drawable.back4);
+                break;
+            case R.id.ll_tab_plan:
+                viewPager.setCurrentItem(3);
+                resetImg();
+                btnPlan.setImageResource(R.drawable.back4);
+                break;
 
+            default:
+                break;
+
+
+        }
+
+
+    }
+
+
+    private void resetImg() {
+        btnMenu.setImageResource(R.drawable.plate);
+        btnMe.setImageResource(R.drawable.person);
+        btnPlan.setImageResource(R.drawable.plan);
+        btnAlarm.setImageResource(R.drawable.alarm);
+
+    }
 
 
 }
